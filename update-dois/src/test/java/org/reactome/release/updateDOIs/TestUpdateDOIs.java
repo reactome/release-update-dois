d6f6ccf47bfc7799891bf36638b70ba4a522804e
Justin Cook <cookersjs@gmail.com>
2018-05-01 09:36:10 -0400

    Basic testing complete; Side effect testing still needs to be done

---
+++ update-dois/src/test/java/org/reactome/release/updateDOIs/TestUpdateDOIs.java
@@ -0,0 +1,87 @@
+package org.reactome.release.updateDOIs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.gk.model.GKInstance;
+import org.gk.persistence.MySQLAdaptor;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.reactome.release.updateDOIs.NewDOIChecker;
+
+//@PrepareForTest({UpdateDOIs.class})
+public class TestUpdateDOIs {
+
+//@PrepareForTest({UpdateDOIs.class})
+	private MySQLAdaptor mockAdaptor = PowerMockito.mock(MySQLAdaptor.class);
+	
+	@Before
+	public void setup() throws Exception
+	{
+		PowerMockito.whenNew(MySQLAdaptor.class).withAnyArguments().thenReturn(mockAdaptor);
+	}
+	
+//	@Test
+//	public void testUpdateDOIs() throws Exception
+//	{
+//		UpdateDOIs check = new UpdateDOIs();
+//	}
+	
+	@Test
+	public void testNewDOIChecker() throws Exception 
+	{
+		NewDOIChecker check = new NewDOIChecker();
+		check.setTestReactomeAdaptor(mockAdaptor);
+		check.setGkCentralAdaptor(mockAdaptor);
+		
+		GKInstance inst1 = PowerMockito.mock(GKInstance.class);
+//		GKInstance inst2 = PowerMockito.mock(GKInstance.class);
+		
+		Collection<GKInstance> testResults = Arrays.asList(inst1);
+		PowerMockito.when(inst1.getAttributeValue("stableIdentifier")).thenReturn("123] 456");
+		PowerMockito.when(inst1.getAttributeValue("name")).thenReturn("Name Value");
+		PowerMockito.when(inst1.getAttributeValue("DB_ID")).thenReturn("67890");
+		
+		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "doi", "NOT REGEXP", "^10.3180")).thenReturn(testResults);
+		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "DB_ID", "=", "67890")).thenReturn(testResults);
+		
+		check.findNewDOIs(12345L);
+    }
+	
+	@Test
+	public void testNewDOICheckerEmptyList() throws Exception 
+	{
+		NewDOIChecker check = new NewDOIChecker();
+		check.setTestReactomeAdaptor(mockAdaptor);
+		check.setGkCentralAdaptor(mockAdaptor);
+		
+		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "doi", "NOT REGEXP", "^10.3180")).thenReturn(new ArrayList<GKInstance>());
+		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "DB_ID", "=", "67890")).thenReturn(new ArrayList<GKInstance>());
+		
+		check.findNewDOIs(12345L);
+	}
+	
+	@Test
+	public void testNewDOICheckerDeepEmptyList() throws Exception 
+	{
+		NewDOIChecker check = new NewDOIChecker();
+		check.setTestReactomeAdaptor(mockAdaptor);
+		check.setGkCentralAdaptor(mockAdaptor);
+		
+		GKInstance inst1 = PowerMockito.mock(GKInstance.class);
+		Collection<GKInstance> testResults = Arrays.asList(inst1);
+		
+		PowerMockito.when(inst1.getAttributeValue("stableIdentifier")).thenReturn("123] 456");
+		PowerMockito.when(inst1.getAttributeValue("name")).thenReturn("Name Value");
+		PowerMockito.when(inst1.getAttributeValue("DB_ID")).thenReturn("67890");
+		
+		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "doi", "NOT REGEXP", "^10.3180")).thenReturn(testResults);
+		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "DB_ID", "=", "67890")).thenReturn(new ArrayList<GKInstance>());
+		
+		check.findNewDOIs(12345L);
+	}
+}
