2859ac8b6b7b7e125f394b7d1886216e13ded293
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-06-14 15:54:52 -0400

    cleanup

--- go-update/src/main/java/org/reactome/goupdate/Main.java
+++ go-update/src/main/java/org/reactome/goupdate/Main.java
@@ -49,18 +49,7 @@ public class Main
 		{
 			// TODO: Read these values from a properties file.
 			MySQLAdaptor adaptor = new MySQLAdaptor("localhost","gk_central","root","root",3306);
-			GKInstance goRefDB = null;
-			try
-			{
-				goRefDB = ((Set<GKInstance>) adaptor.fetchInstanceByAttribute(ReactomeJavaConstants.ReferenceDatabase, ReactomeJavaConstants.name, "=","GO")).stream().findFirst().get();
-				System.out.println("RefDB for GO: "+goRefDB.toString());
-			}
-			catch (Exception e1)
-			{
-				System.out.println("Couldn't even get a reference to the GO ReferenceDatabase object. There's no point in continuing, so this progam will exit. :(");
-				e1.printStackTrace();
-				System.exit(1);
-			}
+
 			try
 			{
 				adaptor.startTransaction();
@@ -77,7 +66,7 @@ public class Main
 			List<String> ec2GoLines = Files.readAllLines(Paths.get(pathToEC2GOFile));
 
 			GoTermsUpdater goTermsUpdator = new GoTermsUpdater(adaptor, goLines, ec2GoLines);
-			
+
 			StringBuilder report = goTermsUpdator.updateGoTerms();
 			System.out.println(report);
 
