6a5103490ee562e5c6f9da45ff00b33dd4e8aa01
Justin Cook <cookersjs@gmail.com>
2018-08-10 11:36:59 -0400

    Tests for comparing Test Reactome and GK Central DOI instances

--- update-dois/src/main/java/org/reactome/release/updateDOIs/ReportTests.java
+++ update-dois/src/main/java/org/reactome/release/updateDOIs/ReportTests.java
@@ -4,13 +4,32 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
+import org.gk.model.GKInstance;
 
 public class ReportTests {
 	
 	final static Logger logger = Logger.getLogger(FindNewDOIsAndUpdate.class);
 	
-	public static void expectedUpdatesTests( HashMap<String,HashMap<String,String>> expectedUpdatedDOIs, ArrayList<String> updatedDOIs, ArrayList<String> updated, ArrayList<String> notUpdated, int fetchHits ) {
-		// Checking if provided list matched updated instances
+	// Compares the DB IDs and display names of the instances to be updated from Test Reactome and GK Central
+	public static boolean verifyDOIMatches( GKInstance trDOI, GKInstance gkDOI, String newDOI ) {
+		if (trDOI.getDBID().equals(gkDOI.getDBID()) && trDOI.getDisplayName().equals(gkDOI.getDisplayName())) {
+			return true;
+		} else if (trDOI.getDBID().equals(gkDOI.getDBID()) && !trDOI.getDisplayName().equals(gkDOI.getDisplayName())) {
+			logger.warn("[" + newDOI + "] Display names do not match: [Test Reactome]: " + trDOI.getDisplayName() + " ~ [GK Central]: " + gkDOI.getDisplayName());
+			return false;
+		} else if (!trDOI.getDBID().equals(gkDOI.getDBID()) && trDOI.getDisplayName().equals(gkDOI.getDisplayName())) {
+			logger.warn("[" + newDOI + "] DB IDs do not match: [Test Reactome]: " + trDOI.getDBID() + " ~ [GK Central]: " + gkDOI.getDBID());
+			return false;
+		} else {
+			logger.warn("DB ID and display name do not match: [Test Reactome]: " + trDOI + " ~ [GK Central]: " + gkDOI);
+			return false;
+		}
+		
+	}
+	
+	public static void expectedUpdatesTests( HashMap<String,HashMap<String,String>> expectedUpdatedDOIs, ArrayList<String> updated, ArrayList<String> notUpdated, int fetchHits ) {
+		// Checking if provided list matched updated instances. Any that don't, it attempts to determine why they might not of been updated.
+		// This entails comparing the DB ID, display name and the stable ID version of the provided list (UpdateDOIs.report) with the actual updated instances
 		if (notUpdated.size() > 0)
 		{
 			
@@ -18,7 +37,6 @@ public class ReportTests {
 			for (String missed : notUpdated) 
 			{
 				String missedDoi = missed.split(":")[0];
-				System.out.println(missedDoi);
 				String missedName = missed.split(":")[1];
 				String missedClean = missedDoi.replace("10.3180/", "");
 				String missedStableId = missedClean.split("\\.")[0];
@@ -30,16 +48,16 @@ public class ReportTests {
 					{
 						if (!expectedUpdatedDOIs.get(key).get("stableIdVersion").equals(missedStableIdVersion)) 
 						{
-							logger.warn("[" + key + "] StableID 'version' in DB different from expected: [DB] " + missedDoi + "* [Expected] " + key + "*");
+							logger.warn("[" + key + "] StableID 'version' in DB different from expected: [DB] " + missedDoi + "* ~ [Expected] " + key + "*");
 							resolved++;
 							continue;
 						} else if (!expectedUpdatedDOIs.get(key).get("displayName").equals(missedName)) {
-							logger.warn("[" + key + "] 'Display name' in DB different from expected: [DB] " + missedName + " [Expected] " + expectedUpdatedDOIs.get(key).get("displayName"));
+							logger.warn("[" + key + "] 'Display name' in DB different from expected: [DB] " + missedName + " ~ [Expected] " + expectedUpdatedDOIs.get(key).get("displayName"));
 							resolved++;
 							continue;
 						}
 					} else if (expectedUpdatedDOIs.get(key).get("displayName").equals(missedName)) {
-						logger.warn("[" + key + "] 'DB ID' from DB different from expected, but found matching display name: [DB] " + missed + " [Expected] " + key + ":" + missedName);
+						logger.warn("[" + key + "] 'DB ID' from DB different from expected, but found matching display name: ~ [DB] " + missed + " [Expected] " + key + ":" + missedName);
 						resolved++;
 						continue;
 					}
@@ -58,7 +76,7 @@ public class ReportTests {
 			}
 		} else if (expectedUpdatedDOIs.size() != 0 && fetchHits > expectedUpdatedDOIs.size()) {
 			logger.warn("The following DOIs were unexpectedly updated: ");
-			for (Object updatedDOI : updatedDOIs)
+			for (Object updatedDOI : updated)
 			{
 				if (expectedUpdatedDOIs.get(updatedDOI) == null)
 				{
