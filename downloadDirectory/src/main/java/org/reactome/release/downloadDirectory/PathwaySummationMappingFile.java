2848a861f25bdd0cb985fe433b2de32a4a17d090
Justin Cook <cookersjs@gmail.com>
2018-10-01 13:28:11 -0400

    Pathway2Summation commenting

--- downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/PathwaySummationMappingFile.java
+++ downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/PathwaySummationMappingFile.java
@@ -15,17 +15,21 @@ public class PathwaySummationMappingFile {
 
 	public static void execute(MySQLAdaptor dba) throws Exception {
 		
+		// Get all Pathway instances
 		Collection<GKInstance> pathwayInstances = dba.fetchInstancesByClass(ReactomeJavaConstants.Pathway);
 		HashSet<String> rowHash = new HashSet<String>();
+		//Create file
 		PrintWriter pathwaySummationFilename = new PrintWriter("pathway2summation.txt");
 		for (GKInstance pathwayInst : pathwayInstances) {
 			boolean containsHumanEntry = false;
+			// Check that at least one of the Species instances is for Homo sapiens
 			for (GKInstance speciesInst: (Collection<GKInstance>) pathwayInst.getAttributeValuesList(ReactomeJavaConstants.species)) {
 				if (speciesInst.getDisplayName().equals("Homo sapiens")) {
 					containsHumanEntry = true;
 				}
 			}
 			if (containsHumanEntry) {
+				// Build line of file
 				String name = pathwayInst.getAttributeValue(ReactomeJavaConstants.name).toString();
 				String stableId = ((GKInstance) pathwayInst.getAttributeValue(ReactomeJavaConstants.stableIdentifier)).getAttributeValue(ReactomeJavaConstants.identifier).toString();
 				for (GKInstance summationInst : (Collection<GKInstance>) pathwayInst.getAttributeValuesList(ReactomeJavaConstants.summation)) {
@@ -33,6 +37,7 @@ public class PathwaySummationMappingFile {
 					text = text.replaceAll(" +", " ");
 					text = text.replaceAll("\n", "");
 					String row = stableId + "\t" + name + "\t" + text + "\n";
+					// Filter duplicates
 					if (!rowHash.contains(row)) {
 						Files.write(Paths.get("pathway2summation.txt"), row.getBytes(), StandardOpenOption.APPEND);
 						rowHash.add(row);
