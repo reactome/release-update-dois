5988c72ce85ecc6474c0e45d96de8a96697f3c7a
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-10-01 17:02:33 -0400

    Make use of org.reactome.release.common.database.InstanceEditUtils

--- chebi-update/src/main/java/org/reactome/release/chebiupdate/ChebiUpdater.java
+++ chebi-update/src/main/java/org/reactome/release/chebiupdate/ChebiUpdater.java
@@ -22,14 +22,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gk.model.GKInstance;
 import org.gk.model.InstanceDisplayNameGenerator;
-import org.gk.model.PersistenceAdaptor;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
 import org.gk.persistence.MySQLAdaptor.AttributeQueryRequest;
 import org.gk.schema.InvalidAttributeException;
 import org.gk.schema.InvalidAttributeValueException;
-import org.gk.schema.SchemaClass;
-import org.gk.util.GKApplicationUtilities;
+import org.reactome.release.common.database.InstanceEditUtils;
 
 import uk.ac.ebi.chebi.webapps.chebiWS.client.ChebiWebServiceClient;
 import uk.ac.ebi.chebi.webapps.chebiWS.model.ChebiWebServiceFault_Exception;
@@ -42,13 +40,12 @@ public class ChebiUpdater
 	private static final Logger refMolNameChangeLog = LogManager.getLogger("molNameChangeLog");
 	private static final Logger refMolIdentChangeLog = LogManager.getLogger("molIdentChangeLog");
 	private static final Logger refEntChangeLog = LogManager.getLogger("refEntChangeLog");
+	private static final Logger duplicatesLog = LogManager.getLogger("duplicatesLog");
 	private boolean testMode = true;
 	private MySQLAdaptor adaptor;
 	private ChebiWebServiceClient chebiClient = new ChebiWebServiceClient();
-//	private StringBuilder identifierSB = new StringBuilder();
 	private StringBuilder formulaUpdateSB = new StringBuilder();
 	private StringBuilder formulaFillSB = new StringBuilder();
-//	private StringBuilder nameSB = new StringBuilder();
 	private StringBuilder duplicatesSB = new StringBuilder();
 	private Map<GKInstance, List<String>> referenceEntityChanges = new HashMap<GKInstance, List<String>>();
 	private long personID;
@@ -129,7 +126,7 @@ public class ChebiUpdater
 		
 		GKInstance instanceEdit = null;
 		adaptor.startTransaction();
-		instanceEdit = createInstanceEdit(this.adaptor, this.personID, this.getClass().getCanonicalName());
+		instanceEdit = InstanceEditUtils.createInstanceEdit(this.adaptor, this.personID, this.getClass().getCanonicalName());
 		for (Long moleculeDBID : entityMap.keySet())
 		{
 			GKInstance molecule = adaptor.fetchInstance(moleculeDBID);
@@ -395,7 +392,8 @@ public class ChebiUpdater
 				+ "having count(ReferenceMolecule.DB_ID) > 1;\n";
 
 		ResultSet duplicates = adaptor.executeQuery(findDuplicateReferenceMolecules, null);
-		logger.info("*** Duplicate ReferenceMolecules ***\n");
+//		logger.info("*** Duplicate ReferenceMolecules ***\n");
+		duplicatesLog.info("# Duplicated Identifier\tReferenceMolecule");
 
 		// Should only be one, but API returns collection.
 		@SuppressWarnings("unchecked")
@@ -417,7 +415,7 @@ public class ChebiUpdater
 			Collection<GKInstance> dupesOfIdentifier = (Collection<GKInstance>) adaptor._fetchInstance(Arrays.asList(chebiAQR, identifierAQR));
 			for (GKInstance duplicate : dupesOfIdentifier)
 			{
-				this.duplicatesSB.append(duplicate.toString()).append("\n");
+				duplicatesLog.info("{}\t{}", identifier, duplicate.toString());
 			}
 		}
 
@@ -560,96 +558,96 @@ public class ChebiUpdater
 		bw.close();
 	}
 
-	/**
-	 * Create an InstanceEdit.
-	 * 
-	 * @param personID
-	 *            - ID of the associated Person entity.
-	 * @param creatorName
-	 *            - The name of the thing that is creating this InstanceEdit.
-	 *            Typically, you would want to use the package and classname that
-	 *            uses <i>this</i> object, so it can be traced to the appropriate
-	 *            part of the program.
-	 * @return
-	 */
-	public GKInstance createInstanceEdit(MySQLAdaptor adaptor, long personID, String creatorName)
-	{
-		GKInstance instanceEdit = null;
-		try
-		{
-			instanceEdit = createDefaultIE(adaptor, personID, true, "Inserted by " + creatorName);
-			instanceEdit.getDBID();
-			adaptor.updateInstance(instanceEdit);
-		}
-		catch (Exception e)
-		{
-			// logger.error("Exception caught while trying to create an InstanceEdit: {}",
-			// e.getMessage());
-			e.printStackTrace();
-		}
-		return instanceEdit;
-	}
-
-	// This code below was taken from 'add-links' repo:
-	// org.reactomeaddlinks.db.ReferenceCreator
-	/**
-	 * Create and save in the database a default InstanceEdit associated with the
-	 * Person entity whose DB_ID is <i>defaultPersonId</i>.
-	 * 
-	 * @param dba
-	 * @param defaultPersonId
-	 * @param needStore
-	 * @return an InstanceEdit object.
-	 * @throws Exception
-	 */
-	public static GKInstance createDefaultIE(MySQLAdaptor dba, Long defaultPersonId, boolean needStore, String note) throws Exception
-	{
-		GKInstance defaultPerson = dba.fetchInstance(defaultPersonId);
-		if (defaultPerson != null)
-		{
-			GKInstance newIE = createDefaultInstanceEdit(defaultPerson);
-			newIE.addAttributeValue(ReactomeJavaConstants.dateTime, GKApplicationUtilities.getDateTime());
-			newIE.addAttributeValue(ReactomeJavaConstants.note, note);
-			InstanceDisplayNameGenerator.setDisplayName(newIE);
-
-			if (needStore)
-			{
-				dba.storeInstance(newIE);
-			}
-			else
-			{
-				logger.info("needStore set to false");
-			}
-			return newIE;
-		}
-		else
-		{
-			throw new Exception("Could not fetch Person entity with ID " + defaultPersonId + ". Please check that a Person entity exists in the database with this ID.");
-		}
-	}
-
-	/**
-	 * Create an InstanceEdit for a specific Person.
-	 * @param person The Person.
-	 * @return An InstanceEdit.
-	 */
-	public static GKInstance createDefaultInstanceEdit(GKInstance person)
-	{
-		GKInstance instanceEdit = new GKInstance();
-		PersistenceAdaptor adaptor = person.getDbAdaptor();
-		instanceEdit.setDbAdaptor(adaptor);
-		SchemaClass cls = adaptor.getSchema().getClassByName(ReactomeJavaConstants.InstanceEdit);
-		instanceEdit.setSchemaClass(cls);
-		try
-		{
-			instanceEdit.addAttributeValue(ReactomeJavaConstants.author, person);
-		}
-		catch (InvalidAttributeException | InvalidAttributeValueException e)
-		{
-			e.printStackTrace();
-			// throw this back up the stack - no way to recover from in here.
-			throw new Error(e);
-		}
-		return instanceEdit;
-	}
+//	/**
+//	 * Create an InstanceEdit.
+//	 * 
+//	 * @param personID
+//	 *            - ID of the associated Person entity.
+//	 * @param creatorName
+//	 *            - The name of the thing that is creating this InstanceEdit.
+//	 *            Typically, you would want to use the package and classname that
+//	 *            uses <i>this</i> object, so it can be traced to the appropriate
+//	 *            part of the program.
+//	 * @return
+//	 */
+//	public GKInstance createInstanceEdit(MySQLAdaptor adaptor, long personID, String creatorName)
+//	{
+//		GKInstance instanceEdit = null;
+//		try
+//		{
+//			instanceEdit = createDefaultIE(adaptor, personID, true, "Inserted by " + creatorName);
+//			instanceEdit.getDBID();
+//			adaptor.updateInstance(instanceEdit);
+//		}
+//		catch (Exception e)
+//		{
+//			// logger.error("Exception caught while trying to create an InstanceEdit: {}",
+//			// e.getMessage());
+//			e.printStackTrace();
+//		}
+//		return instanceEdit;
+//	}
+//
+//	// This code below was taken from 'add-links' repo:
+//	// org.reactomeaddlinks.db.ReferenceCreator
+//	/**
+//	 * Create and save in the database a default InstanceEdit associated with the
+//	 * Person entity whose DB_ID is <i>defaultPersonId</i>.
+//	 * 
+//	 * @param dba
+//	 * @param defaultPersonId
+//	 * @param needStore
+//	 * @return an InstanceEdit object.
+//	 * @throws Exception
+//	 */
+//	public static GKInstance createDefaultIE(MySQLAdaptor dba, Long defaultPersonId, boolean needStore, String note) throws Exception
+//	{
+//		GKInstance defaultPerson = dba.fetchInstance(defaultPersonId);
+//		if (defaultPerson != null)
+//		{
+//			GKInstance newIE = createDefaultInstanceEdit(defaultPerson);
+//			newIE.addAttributeValue(ReactomeJavaConstants.dateTime, GKApplicationUtilities.getDateTime());
+//			newIE.addAttributeValue(ReactomeJavaConstants.note, note);
+//			InstanceDisplayNameGenerator.setDisplayName(newIE);
+//
+//			if (needStore)
+//			{
+//				dba.storeInstance(newIE);
+//			}
+//			else
+//			{
+//				logger.info("needStore set to false");
+//			}
+//			return newIE;
+//		}
+//		else
+//		{
+//			throw new Exception("Could not fetch Person entity with ID " + defaultPersonId + ". Please check that a Person entity exists in the database with this ID.");
+//		}
+//	}
+//
+//	/**
+//	 * Create an InstanceEdit for a specific Person.
+//	 * @param person The Person.
+//	 * @return An InstanceEdit.
+//	 */
+//	public static GKInstance createDefaultInstanceEdit(GKInstance person)
+//	{
+//		GKInstance instanceEdit = new GKInstance();
+//		PersistenceAdaptor adaptor = person.getDbAdaptor();
+//		instanceEdit.setDbAdaptor(adaptor);
+//		SchemaClass cls = adaptor.getSchema().getClassByName(ReactomeJavaConstants.InstanceEdit);
+//		instanceEdit.setSchemaClass(cls);
+//		try
+//		{
+//			instanceEdit.addAttributeValue(ReactomeJavaConstants.author, person);
+//		}
+//		catch (InvalidAttributeException | InvalidAttributeValueException e)
+//		{
+//			e.printStackTrace();
+//			// throw this back up the stack - no way to recover from in here.
+//			throw new Error(e);
+//		}
+//		return instanceEdit;
+//	}
 }
