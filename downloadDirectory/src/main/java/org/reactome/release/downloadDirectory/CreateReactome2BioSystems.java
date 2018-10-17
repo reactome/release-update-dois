2aaa2975f8a7d7cd3788c7318e035ccd47f5aaa1
cookersjs <cookersjs@gmail.com>
2018-10-17 13:20:25 -0400

    CreateReactome2BioSystems step finished

---
+++ downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/CreateReactome2BioSystems.java
@@ -0,0 +1,16 @@
+package org.reactome.release.downloadDirectory;
+
+import java.io.IOException;
+
+import org.gk.biosystems.ReactomeToBioSystemsConverter;
+
+public class CreateReactome2BioSystems {
+
+	public static void execute(String host, String database, String username, String password, int port) throws IOException {
+		// The last argument, 'BioSystems', specifies the output directory of the ReactomeToBioSystems.zip file. 
+		// The script removes all files within the directory, so only change the output directory to an empty or non-existent one
+		ReactomeToBioSystemsConverter.main(new String[] {host, database, username, password, Integer.toString(port), "BioSystems" });
+		Runtime.getRuntime().exec("mv BioSystems/ReactomeToBioSystems.zip .");
+		Runtime.getRuntime().exec("rm -r BioSystems");
+	}
+}
--- downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/Main.java
+++ downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		props.load(new FileInputStream(pathToConfig));
 		
 		//TODO: Check stable identifiers db exists; 
-		
+		//TODO: Describe each functions outputs
 		//Set up DB adaptor
 		Object speciesToInferFromLong = "Homo sapiens";
 		String username = props.getProperty("username");
@@ -40,12 +40,13 @@ public class Main {
 //		MapOldStableIds.execute(username, password, releaseNumber);
 		
 		// These file copy commands now use absolute paths instead of relative ones
-		String releaseDirAbsolute = "/usr/local/gkb/scripts/release";
-		Process copyGeneAssociationFile = Runtime.getRuntime().exec("cp " + releaseDirAbsolute + "/goa_prepare/gene_association.reactome .");
-		copyGeneAssociationFile.waitFor();
-		Process copyModels2PathwaysFile = Runtime.getRuntime().exec("cp " + releaseDirAbsolute + "/biomodels/models2pathways.tsv .");
-		copyModels2PathwaysFile.waitFor();
+//		String releaseDirAbsolute = "/usr/local/gkb/scripts/release";
+//		Process copyGeneAssociationFile = Runtime.getRuntime().exec("cp " + releaseDirAbsolute + "/goa_prepare/gene_association.reactome .");
+//		copyGeneAssociationFile.waitFor();
+//		Process copyModels2PathwaysFile = Runtime.getRuntime().exec("cp " + releaseDirAbsolute + "/biomodels/models2pathways.tsv .");
+//		copyModels2PathwaysFile.waitFor();
 		
-		//TODO: create_reactome2biosystems
+		CreateReactome2BioSystems.execute(host, database, username, password, port);
+		
 	}
 }
