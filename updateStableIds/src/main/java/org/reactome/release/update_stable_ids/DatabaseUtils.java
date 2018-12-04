f213eb54575ee2397983a015eccad227b3aa009b
cookersjs <cookersjs@gmail.com>
2018-12-04 15:05:10 -0500

    Initial commit; DB Adaptors for slice and gk_central

---
+++ updateStableIds/src/main/java/org/reactome/release/update_stable_ids/DatabaseUtils.java
@@ -0,0 +1,11 @@
+package org.reactome.release.update_stable_ids;
+
+public class DatabaseUtils {
+
+	public static void archiveUsedDatabases(String sliceDb, String gkCentralDb) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(sliceDb + "\t" + gkCentralDb);
+	}
+
+}
---
+++ updateStableIds/src/main/java/org/reactome/release/update_stable_ids/Main.java
@@ -0,0 +1,51 @@
+package org.reactome.release.update_stable_ids;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.gk.persistence.MySQLAdaptor;
+
+/**
+ * This function iterates through all instances, and checks if it has been changed since the previous release.
+ * Instances that have been changed have their 'identifierVersion' attribute incremented by 1 to reflect a change. 
+ *
+ */
+public class Main 
+{
+	private static Connection connect = null;
+	private static Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private static ResultSet resultSet = null;
+
+	
+    public static void main( String[] args ) throws SQLException, FileNotFoundException, IOException
+    {
+       DatabaseUtils.archiveUsedDatabases("test_slice_67", "gk_central");
+       
+       String pathToConfig = "src/main/resources/config.properties";
+       
+       Properties props = new Properties();
+       props.load(new FileInputStream(pathToConfig));
+       int port = Integer.valueOf(props.getProperty("port"));
+       
+       String sliceUsername = props.getProperty("sliceUsername");
+       String slicePassword = props.getProperty("slicePassword");
+       String sliceDatabase = props.getProperty("sliceDatabase");
+       String sliceHost = props.getProperty("sliceHost");	   
+       MySQLAdaptor dbaSlice = new MySQLAdaptor(sliceHost, sliceDatabase, sliceUsername, slicePassword, port);
+       
+       String gkCentralUsername = props.getProperty("gkCentralUsername");
+       String gkCentralPassword = props.getProperty("gkCentralPassword");
+       String gkCentralDatabase = props.getProperty("gkCentralDatabase");
+       String gkCentralHost = props.getProperty("gkCentralHost");
+       
+       MySQLAdaptor dbaGkCentral = new MySQLAdaptor(gkCentralHost, gkCentralDatabase, gkCentralUsername, gkCentralPassword, port);
+;    }
+}
