56e06480ae06c12db072236be67718c04574a879
cookersjs <cookersjs@gmail.com>
2018-12-17 10:39:39 -0500

    Added descriptive comments for each d_d step

--- downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/Main.java
+++ downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/Main.java
@@ -68,52 +68,75 @@ public class Main {
 		}
 		br.close();
 		
-		//Begin download directory
+		//Begin download directory steps
 		if (stepsToRun.contains("DatabaseDumps")) 
 		{
+			// This step takes a DB Dump of the stable_identifiers and test_reactome DBs
+			// Outputs: gk_stable_ids.sql, gk_current.sql
 			DatabaseDumps.execute(dbAdaptor, releaseNumber, username, password, host, port, database);
 		}
 		if (stepsToRun.contains("BioPAX2") || stepsToRun.contains("BioPAX3")) 
 		{
-			Biopax.execute(username, password, host, Integer.toString(port), database, releaseNumber, pathToSpeciesConfig, stepsToRun.contains("BioPAX2"), stepsToRun.contains("BioPAX3"));
+			// This step runs BioPAX level 2 and BioPAX level 3 for Reactome's data using the Pathway-Exchange functinos
+			// Outputs: biopax2.zip and biopax2_validator.zip, and biopax.zip and biopax_validator.zip (for level 3)
+			// These zip files should contain a number of 'owl' files (BioPAX files) and 'xml' validation files
+//			Biopax.execute(username, password, host, Integer.toString(port), database, releaseNumber, pathToSpeciesConfig, stepsToRun.contains("BioPAX2"), stepsToRun.contains("BioPAX3"));
 		}
 		if (stepsToRun.contains("GSEAOutput")) 
 		{
+			// This step converts all Human Pathways to the MSigDB format used by GSEA
+			// Output: ReactomePathways.gmt.zip
 			GSEAOutput.execute(dbAdaptor, releaseNumber);
 		}
 		if (stepsToRun.contains("ReactomeBookPDF") || stepsToRun.contains("ReactomeBookRTF")) 
 		{
-			ReactomeBookGenerator.execute(username, password, host, port, database, releaseNumber, releaseDownloadDir, stepsToRun.contains("ReactomeBookPDF"), stepsToRun.contains("ReactomeBookRTF"));
+			// This step currently calls the Perl scripts that generate the ReactomeBookPDF and ReactomeBookRTF
+			//Outputs: TheReactomeBook.pdf.zip, TheReactomeBook.rtf.zip
+//			ReactomeBookGenerator.execute(username, password, host, port, database, releaseNumber, releaseDownloadDir, stepsToRun.contains("ReactomeBookPDF"), stepsToRun.contains("ReactomeBookRTF"));
 		}
 		if (stepsToRun.contains("FetchTestReactomeOntologyFiles")) 
 		{
+			// This step, (formerly fetchEmptyProject), takes the blob output from the Ontology.ontology and parses it into 3 files
+			// Outputs: reactome_data_model.pprj, reactome_data_model.pont, reactome_data_model.pins
 			FetchTestReactomeOntologyFiles.execute(dbAdaptor, username, password, host, database, releaseNumber);
 		}
 		if (stepsToRun.contains("CreateReleaseTarball")) 
 		{
-			CreateReleaseTarball.execute(releaseNumber, releaseDownloadDir);
+			// This step clones the Release repo from github, and generates an archive tarball from it and other files on the release server. Currently just runs make_release_tarball.pl.
+			// Output: reactome.tar.gz 
+//			CreateReleaseTarball.execute(releaseNumber, releaseDownloadDir);
 		}
 		if (stepsToRun.contains("PathwaySummationMappingFile")) 
 		{
+			// This step takes all Human Pathway and creates a tab-seperated file with columns containining the stableIdentifier, name, and summation of the instance
+			// Output: pasthway2summation.txt
 			PathwaySummationMappingFile.execute(dbAdaptor, releaseNumber);
 		}
 		if (stepsToRun.contains("MapOldStableIds")) 
 		{
+			// This step iterates through all StableIdentifiers and maps them to the old Reactome ID in 'REACT_#####' format. Human instances are displayed first.
+			// Output: reactome_stable_ids.txt
 			MapOldStableIds.execute(username, password, host, releaseNumber);
 		}
 		// These file copy commands now use absolute paths instead of relative ones
 		if (stepsToRun.contains("gene_association.reactome")) 
 		{
+			// This step copies the gene_association.reactome file generated during the goa_prepare step of Release to the download_directory folder
+			// Output: gene_association.reactome
 			logger.info("Copying gene_association.reactome to release directory");
 			Files.copy(Paths.get(releaseDirAbsolute + "goa_prepare/gene_association.reactome"), Paths.get(releaseNumber + "gene_association.reactome"), StandardCopyOption.REPLACE_EXISTING);
 		}
 		if (stepsToRun.contains("models2pathways.tsv"))
 		{
+			// This step copies the models2pathways.tsv file generated during the biomodels step of Release to the download_directory folder
 			logger.info("Copying models2pathways.tsv to release directory");
 			Files.copy(Paths.get(releaseDirAbsolute + "biomodels/models2pathways.tsv"), Paths.get(releaseNumber + "models2pathways.tsv"), StandardCopyOption.REPLACE_EXISTING);
 		}
 		if (stepsToRun.contains("CreateReactome2BioSystems")) 
 		{
+			// This step converts Reactome Pathway instances into the NCBI BioSystems format (http://www.ncbi.nlm.nih.gov/biosystems/). 
+			// Output: ReactomeToBioSystems.zip
+			// This zip file contains an 'xml' file for each species in Reactome in BioSystems format 
 			CreateReactome2BioSystems.execute(host, database, username, password, port, releaseNumber);
 		}
 		// Move files to downloadDirectory release folder
