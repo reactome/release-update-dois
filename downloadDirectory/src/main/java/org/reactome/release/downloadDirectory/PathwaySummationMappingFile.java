524016b3a48e7323c24f6e582b65dc62154a1833
cookersjs <cookersjs@gmail.com>
2018-12-21 14:22:41 -0500

    Cleaned up use of ReactomeJavaConstants

--- downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/PathwaySummationMappingFile.java
+++ downloadDirectory/src/main/java/org/reactome/release/downloadDirectory/PathwaySummationMappingFile.java
@@ -1,7 +1,6 @@
 package org.reactome.release.downloadDirectory;
 
 import java.io.File;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -13,7 +12,7 @@ import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gk.model.GKInstance;
-import org.gk.model.ReactomeJavaConstants;
+import static org.gk.model.ReactomeJavaConstants.*;
 import org.gk.persistence.MySQLAdaptor;
 
 public class PathwaySummationMappingFile {
@@ -25,7 +24,7 @@ public class PathwaySummationMappingFile {
 		
 		logger.info("Running PathwaySummationMappingFile step");
 		// Get all Pathway instances
-		Collection<GKInstance> pathwayInstances = dba.fetchInstancesByClass(ReactomeJavaConstants.Pathway);
+		Collection<GKInstance> pathwayInstances = dba.fetchInstancesByClass(Pathway);
 		Set<String> rowHash = new HashSet<String>();
 		//Create file
 		logger.info("Generating pathway2summation.txt file...");
@@ -36,7 +35,7 @@ public class PathwaySummationMappingFile {
 		{
 			boolean containsHumanEntry = false;
 			// Check that at least one of the Species instances is for Homo sapiens
-			for (GKInstance speciesInst: (Collection<GKInstance>) pathwayInst.getAttributeValuesList(ReactomeJavaConstants.species)) 
+			for (GKInstance speciesInst: (Collection<GKInstance>) pathwayInst.getAttributeValuesList(species)) 
 			{
 				if (speciesInst.getDisplayName().equals("Homo sapiens")) 
 				{
@@ -46,12 +45,12 @@ public class PathwaySummationMappingFile {
 			if (containsHumanEntry) 
 			{
 				// Build line of file
-				String name = pathwayInst.getAttributeValue(ReactomeJavaConstants.name).toString();
-				String stableId = ((GKInstance) pathwayInst.getAttributeValue(ReactomeJavaConstants.stableIdentifier)).getAttributeValue(ReactomeJavaConstants.identifier).toString();
-				for (GKInstance summationInst : (Collection<GKInstance>) pathwayInst.getAttributeValuesList(ReactomeJavaConstants.summation)) 
+				String nameString = pathwayInst.getAttributeValue(name).toString();
+				String stableId = ((GKInstance) pathwayInst.getAttributeValue(stableIdentifier)).getAttributeValue(identifier).toString();
+				for (GKInstance summationInst : (Collection<GKInstance>) pathwayInst.getAttributeValuesList(summation)) 
 				{
-					String text = summationInst.getAttributeValue(ReactomeJavaConstants.text).toString().replaceAll(" +", " ").replaceAll("\n", "");
-					String row = stableId + "\t" + name + "\t" + text + "\n";
+					String textString = summationInst.getAttributeValue(text).toString().replaceAll(" +", " ").replaceAll("\n", "");
+					String row = stableId + "\t" + nameString + "\t" + textString + "\n";
 					// Filter duplicates
 					if (!rowHash.contains(row)) 
 					{
