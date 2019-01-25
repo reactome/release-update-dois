86316fd50637b5ce3f89a9a2cf102218dd718606
cookersjs <cookersjs@gmail.com>
2019-01-25 09:53:27 -0500

    Refresh of biomartless orthopairs: all file downloads

--- orthopairs/src/main/java/org/reactome/release/orthopairs/Main.java
+++ orthopairs/src/main/java/org/reactome/release/orthopairs/Main.java
@@ -1,8 +1,11 @@
 package org.reactome.release.orthopairs;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.compress.archivers.ArchiveEntry;
@@ -32,23 +35,30 @@ public class Main
 
         String releaseNumber = props.get("releaseNumber").toString();
         String pathToSpeciesConfig = props.get("pathToSpeciesConfig").toString();
-        String pathToXMLQuery = props.get("pathToXMLQuery").toString();
         String pantherFilepath = props.get("pantherFileURL").toString();
         String pantherQfOFilename = props.get("pantherQfOFilename").toString();
         String pantherHCOPFilename = props.get("pantherHCOPFilename").toString();
+        String HGNCFileURL = props.get("HGNCFileURL").toString();
+        String MGIFileURL = props.get("MGIFileURL").toString();
+        String RGDFileURL = props.get("RGDFileURL").toString();
+        String XenbaseFileURL = props.get("XenbaseFileURL").toString();
+        String ZFINFileURL = props.get("ZFINFileURL").toString();
+
+//        new File(releaseNumber).mkdir();
 
         List<String> pantherFiles = new ArrayList<String>(Arrays.asList(pantherQfOFilename, pantherHCOPFilename));
-
         for (String pantherFilename : pantherFiles) {
-            URL pantherFileURL = new URL(pantherFilepath + pantherFilename);
-            String pantherFilenameWithRelease = releaseNumber + "_" + pantherFilename;
-            File pantherFileTar = new File(pantherFilenameWithRelease);
+            downloadAndExtractTarFile(pantherFilename, pantherFilepath);
+        }
 
-            if (!pantherFileTar.exists()) {
-                System.out.println("Downloading " + pantherFileURL);
-                FileUtils.copyURLToFile(pantherFileURL, new File(pantherFilenameWithRelease));
+        List<String> alternativeIdMappingURLs = new ArrayList<>(Arrays.asList(HGNCFileURL, MGIFileURL,RGDFileURL,XenbaseFileURL,ZFINFileURL));
+        for (String altIdURL : alternativeIdMappingURLs) {
+            File altIdFilepath = new File(altIdURL.substring(altIdURL.lastIndexOf("/")+1));
+            if (!altIdFilepath.exists()) {
+                System.out.println("Downloading " + altIdURL);
+                FileUtils.copyURLToFile(new URL(altIdURL), altIdFilepath);
             } else {
-                System.out.println(pantherFileTar + " already exists");
+                System.out.println(altIdFilepath + " already exists");
             }
         }
 
@@ -62,145 +72,38 @@ public class Main
         } else {
             sourceMappingSpecies = "hsap";
         }
-
-        Set<String> pantherSpeciesNames = new HashSet<>();
-        for (Object speciesKey : speciesJSONFile.keySet()) {
-            JSONObject speciesJSONObject = (JSONObject) speciesJSONFile.get(speciesKey);
-            String speciesName = (String) ((JSONArray) speciesJSONObject.get("name")).get(0);
-            String biomartFilename = releaseNumber + "_" + speciesKey;
-            String alternativeIdsFilename = releaseNumber + "_" + speciesKey;
-            if (speciesKey.equals(sourceMappingSpecies)) {
-                biomartFilename += "_protein_gene_mapping.txt";
-                alternativeIdsFilename += "_altId_ensembl_mapping.txt";
-            } else {
-                biomartFilename += "_gene_protein_mapping.txt";
-                alternativeIdsFilename += "_altId_ensembl_mapping.txt";
-                pantherSpeciesNames.add(speciesJSONObject.get("panther_name").toString());
-            }
-
-            File biomartFile = new File(biomartFilename);
-            File altIdFile = new File(alternativeIdsFilename);
-
-            boolean altIdFileNeeded = false;
-            if (speciesJSONObject.get("panther_gene_dbs") != null && !altIdFile.exists()) {
-                altIdFileNeeded = true;
-            }
-
-            if (!biomartFile.exists() || altIdFileNeeded) {
-                System.out.println("Generating Biomart mapping files for " + speciesName);
-                // Build the XML portion of the Biomart query
-                String biomartXMLQuery = BiomartUtilities.buildBiomartXML((String) speciesKey, (JSONObject) speciesJSONFile.get(speciesKey), pathToXMLQuery);
-                // Query Biomart for gene-protein mappings for species
-                List<String> biomartResults = BiomartUtilities.queryBiomart((String) speciesKey, (JSONObject) speciesJSONFile.get(speciesKey), biomartXMLQuery);
-                // Generate Orthopairs Mapping Files
-                if (speciesKey.equals(sourceMappingSpecies)) {
-                    MappingFileGenerator.createSourceProteinGeneMappingFile(biomartResults, (String) speciesKey, speciesName, biomartFile, altIdFile);
-                } else {
-                    MappingFileGenerator.createTargetGeneProteinMappingFile(biomartResults, (String) speciesKey, speciesName, biomartFile, altIdFile);
-                }
-            } else {
-                System.out.println("Biomart mapping files already exist");
-            }
-        }
-
-        // PIG is found only in the Ortholog_HCOP file, while the rest are found in the QfO_Genome_Orthologs.
-        // We need to go through both files to get all information. The exact same information is found for species
-        // found in both files. Sets are used to prevent redundancy.
-        for (String pantherFilename : pantherFiles) {
-            // First, extract the tar file if needed.
-            String pantherFilenameWithRelease = releaseNumber + "_" + pantherFilename;
-            File pantherFileTar = new File(pantherFilenameWithRelease);
-            File extractedPantherFile = new File(pantherFilename.replace(".tar.gz", ""));
-            if (!extractedPantherFile.exists()) {
-                InputStream is = new FileInputStream(pantherFileTar);
-                GzipCompressorInputStream gzipIn = new GzipCompressorInputStream(is);
-                TarArchiveInputStream tarIn = new TarArchiveInputStream(gzipIn);
-                TarArchiveEntry tarFile;
-                while ((tarFile = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
-                    int count;
-                    byte data[] = new byte[1024];
-                    FileOutputStream fos = new FileOutputStream(tarFile.getName(), false);
-                    try (BufferedOutputStream dest = new BufferedOutputStream(fos, 1024)) {
-                        while ((count = tarIn.read(data, 0, 1024)) != -1) {
-                            dest.write(data, 0, count);
-                        }
-                    }
-                    //TODO: Version output file
-                }
-            } else {
-                System.out.println("Panther file has already been extracted");
-            }
-
-            // Panther has specific names for each organism that are found in the Species config
-            JSONObject sourceJSONObject = (JSONObject) speciesJSONFile.get(sourceMappingSpecies);
-            String sourceSpeciesName = (String) sourceJSONObject.get("panther_name");
-
-            // Iterate through each line of the file
-            Map<String, Map<String,Set<String>>> homologGenes = new HashMap<>();
-            Map<String, Set<String>> homlogProteins = new HashMap<>();
-            String line;
-            BufferedReader br = new BufferedReader(new FileReader(extractedPantherFile));
-            while ((line = br.readLine()) != null) {
-                // Filter for only Reactome's species
-                if (line.startsWith(sourceSpeciesName)) {
-                    // Example line: HUMAN|HGNC=10663|UniProtKB=O60524       MOUSE|MGI=MGI=1918305|UniProtKB=Q8CCP0  LDO     Euarchontoglires        PTHR15239
-                    // 5 tab-seperated columns. The first and second pertain to the source and target species, respectively. These two columns can be further divided by '|',
-                    // giving the species Name, geneId and its source DB, and proteinId and its source DB. The third tab-seperated column gives the ortholog type (LDO = Least Diverged Ortholog, O = Ortholog).
-                    // Fourth column is last common ancestor or clade between source and target (verify). Fifth column is Panther ID.
-                    // We only parse the first three columns.
-                    String[] tabSplit = line.split("\t");
-                    String sourceGeneInfo = tabSplit[0].split("\\|")[1];
-                    String targetInfo = tabSplit[1];
-                    String orthologType = tabSplit[2];
-                    String[] targetSplit = targetInfo.split("\\|");
-
-                    // Gene IDs that are labelled with 'Gene|GeneID|Gene_Name|Gene_ORFName|Gene_OrderedLocusName' are not traceable to specific resource, so are ignored.
-                    //TODO: This might not apply to Proteins?
-                    if (pantherSpeciesNames.contains(targetSplit[0]) && !sourceGeneInfo.startsWith("Gene") && ((orthologType.equals("LDO") || orthologType.equals("O")))) {
-                        String targetSpeciesName = targetSplit[0];
-                        String targetGeneInfo = targetSplit[1];
-                        // Lines with LDO are the most important. If a human gene has an LDO ortholog with the species, then it is the only ortholog we keep.
-                        // Rarely there are multiple LDOs, and in those cases we keep both.
-                        if (homologGenes.get(targetSpeciesName) == null) {
-                            Map<String,Set<String>> firstHumanSpeciesGeneMap = new HashMap<>();
-                            Set<String> firstSpeciesGene = new HashSet<>(Arrays.asList(targetGeneInfo));
-                            if (orthologType.equals("LDO")) {
-                                firstSpeciesGene.add("LDO");
-                            }
-                            firstHumanSpeciesGeneMap.put(sourceGeneInfo, firstSpeciesGene);
-                            homologGenes.put(targetSpeciesName, firstHumanSpeciesGeneMap);
-                        } else {
-                            if (homologGenes.get(targetSpeciesName).get(sourceGeneInfo) == null) {
-                                Set<String> firstSpeciesGene = new HashSet<>(Arrays.asList(targetGeneInfo));
-                                if (orthologType.equals("LDO")) {
-                                    firstSpeciesGene.add("LDO");
-                                }
-                                homologGenes.get(targetSpeciesName).put(sourceGeneInfo, firstSpeciesGene);
-                            } else {
-                                Set<String> speciesGeneSet = homologGenes.get(targetSpeciesName).get(sourceGeneInfo);
-                                if (speciesGeneSet.contains("LDO")) {
-                                    // Human gene will have multiple LDO's
-                                    if (orthologType.equals("LDO")) {
-                                        homologGenes.get(targetSpeciesName).get(sourceGeneInfo).add(targetGeneInfo);
-
-                                    }
-                                } else if (orthologType.equals("LDO")) {
-                                    speciesGeneSet.clear();
-                                    speciesGeneSet.add(targetGeneInfo);
-                                    speciesGeneSet.add("LDO");
-                                    homologGenes.get(targetSpeciesName).put(sourceGeneInfo, speciesGeneSet);
-                                } else {
-                                    homologGenes.get(targetSpeciesName).get(sourceGeneInfo).add(targetGeneInfo);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Having iterated through both files, we now have two structures: Source species Genes and their target species orthologs, and the protein counterpart.
-            // We will now write these results to a file before mapping them with the Biomart files.
-        }
     }
 
+    private static void downloadAndExtractTarFile(String pantherFilename, String pantherFilepath) throws IOException {
+
+        URL pantherFileURL = new URL(pantherFilepath + pantherFilename);
+        File pantherTarFile = new File(pantherFilename);
+
+        if (!pantherTarFile.exists()) {
+            System.out.println("Downloading " + pantherFileURL);
+            FileUtils.copyURLToFile(pantherFileURL, new File(pantherFilename));
+        } else {
+            System.out.println(pantherTarFile + " already exists");
+        }
+
+        // Download and extract Panther tar files
+        File extractedPantherFile = new File(pantherFilename.replace(".tar.gz", ""));
+        if (!extractedPantherFile.exists()) {
+            System.out.println("Extracting " + pantherTarFile);
+            TarArchiveInputStream tarIn = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(pantherTarFile)));
+            TarArchiveEntry tarFile;
+            while ((tarFile = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
+                int count;
+                byte data[] = new byte[1024];
+                FileOutputStream fos = new FileOutputStream(tarFile.getName(), false);
+                try (BufferedOutputStream dest = new BufferedOutputStream(fos, 1024)) {
+                    while ((count = tarIn.read(data, 0, 1024)) != -1) {
+                        dest.write(data, 0, count);
+                    }
+                }
+            }
+        } else {
+            System.out.println(pantherTarFile + " has already been extracted");
+        }
+    }
 }
