3a4be80bf2bb9df335abd8a050075972a01f6449
Joel Weiser <joel.weiser@oicr.on.ca>
2019-01-28 15:20:16 -0500

    Enables git pull and mvn compile

--- orthoinference/runOrthoinference.sh
+++ orthoinference/runOrthoinference.sh
@@ -1,7 +1,7 @@
 ## Update repo
-#git pull
+git pull
 ## Create new jar file with orthoinference code
-#mvn clean compile assembly:single
+mvn clean compile assembly:single
 
 ## Run orthoinference for each species
 allSpecies=(pfal spom scer ddis cele sscr btau cfam mmus rnor ggal tgut xtro drer dmel atha osat)
