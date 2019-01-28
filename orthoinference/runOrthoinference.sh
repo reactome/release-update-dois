3c61eea8f544f996dc6555b030db265b8756a3eb
cookersjs <cookersjs@gmail.com>
2019-01-28 11:51:44 -0500

    Removed -Xmx command from bash script

--- orthoinference/runOrthoinference.sh
+++ orthoinference/runOrthoinference.sh
@@ -1,14 +1,14 @@
 ## Update repo
-git pull
+#git pull
 ## Create new jar file with orthoinference code
-mvn clean compile assembly:single
+#mvn clean compile assembly:single
 
 ## Run orthoinference for each species
 allSpecies=(pfal spom scer ddis cele sscr btau cfam mmus rnor ggal tgut xtro drer dmel atha osat)
 for species in "${allSpecies[@]}"
 do
-	echo "java -Xmx4096m -jar target/orthoinference-0.0.1-SNAPSHOT-jar-with-dependencies.jar $species > orthoinference_$species.out";
-	java -Xmx4096m -jar target/orthoinference-0.0.1-SNAPSHOT-jar-with-dependencies.jar $species 
+	echo "java -jar target/orthoinference-0.0.1-SNAPSHOT-jar-with-dependencies.jar $species > orthoinference_$species.out";
+	java -jar target/orthoinference-0.0.1-SNAPSHOT-jar-with-dependencies.jar $species > orthoinference.out;
 done
 echo "Orthoinference complete"
 
