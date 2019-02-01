1a9dbf4274d4e53daff0cdc4d8bcc3092b286665
Joel Weiser <joel.weiser@oicr.on.ca>
2019-02-01 12:39:01 -0500

    Builds and locally maven installs the pathwayExchange.jar

--- downloadDirectory/runDownloadDirectory.sh
+++ downloadDirectory/runDownloadDirectory.sh
@@ -4,6 +4,10 @@ cd $DIR
 
 ## Make sure the repo is up to date
 git pull
+
+## Execute script to build and locally install pathwayExchange.jar
+./build_pathway_exchange.sh
+
 ## Generate the jar file and run the Download Directory program
 mvn clean package
 unzip -o target/downloadDirectory-distr.zip
