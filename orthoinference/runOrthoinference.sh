0136d2e88987ec3dd22dba21a00cc74b0d1fc151
cookersjs <cookersjs@gmail.com>
2019-02-14 09:28:55 -0500

    Removed species from bash script

--- orthoinference/runOrthoinference.sh
+++ orthoinference/runOrthoinference.sh
@@ -8,7 +8,7 @@ git pull
 mvn clean compile assembly:single
 
 ## Run orthoinference for each species
-allSpecies=(pfal spom scer ddis cele sscr btau cfam mmus rnor ggal tgut xtro drer dmel atha osat)
+allSpecies=(pfal spom scer ddis cele sscr btau cfam mmus rnor ggal xtro drer dmel)
 for species in "${allSpecies[@]}"
 do
 	echo "java -jar target/orthoinference-0.0.1-SNAPSHOT-jar-with-dependencies.jar $species > orthoinference_$species.out";
