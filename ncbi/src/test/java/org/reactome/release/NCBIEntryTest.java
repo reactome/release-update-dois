acfd89f9905384dd2886f1664389e64a519f5d1c
Joel Weiser <joel.weiser@oicr.on.ca>
2019-03-24 01:26:27 -0400

    creates test templates

---
+++ ncbi/src/test/java/org/reactome/release/NCBIEntryTest.java
@@ -0,0 +1,12 @@
+package org.reactome.release;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NCBIEntryTest {
+
+	@Test
+	public void getTopLevelPathways() {
+	}
+}
---
+++ ncbi/src/test/java/org/reactome/release/NCBITest.java
@@ -0,0 +1,7 @@
+package org.reactome.release;
+
+import static org.junit.Assert.*;
+
+public class NCBITest {
+
+}
