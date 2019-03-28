4311f90a018e483e91d6c1a1632c167a48faf059
Joel Weiser <joel.weiser@oicr.on.ca>
2019-03-28 14:22:33 -0400

    adds UCSC file generation

--- ncbi/src/main/java/org/reactome/release/NCBI.java
+++ ncbi/src/main/java/org/reactome/release/NCBI.java
@@ -49,6 +49,9 @@ public class NCBI {
 		System.out.println("Writing " + getNCBIProteinFilePath().getFileName());
 		writeNCBIProteinFile();
 
+		System.out.println("Writing UCSC files");
+		writeUCSCFiles(graphDBSession);
+
 		graphDBSession.close();
 		logger.info("Finished NCBI step");
 		System.exit(0);
@@ -204,6 +207,87 @@ public class NCBI {
 	private static String getProteinFileSeparator() {
 		final int TIMES_TO_REPEAT = 56;
 		return StringUtils.repeat('-', TIMES_TO_REPEAT);
+	}
+
+	private static void writeUCSCFiles(Session graphDBSession) throws IOException {
+		List<UniProtReactomeEntry> uniProtReactomeEntries = graphDBSession.run(
+			String.join(System.lineSeparator(),
+				"MATCH (ewas:EntityWithAccessionedSequence)-[:referenceEntity]->(rgp:ReferenceGeneProduct)" +
+				"-[:referenceDatabase]->(rd:ReferenceDatabase)",
+				"WHERE ewas.speciesName IN ['Homo sapiens', 'Rattus norvegicus', 'Mus musculus'] AND rd.displayName =" +
+				" 'UniProt'",
+				"RETURN rgp.dbId, rgp.identifier, rgp.displayName"
+			)
+		)
+		.stream()
+		.map(record -> UniProtReactomeEntry.get(
+			record.get("rgp.dbId").asLong(),
+			record.get("rgp.identifier").asString(),
+			record.get("rgp.displayName").asString()
+		))
+		.collect(Collectors.toList());
+
+		writeUCSCEntityFile(uniProtReactomeEntries);
+		writeUCSCEventFile(graphDBSession, uniProtReactomeEntries);
+	}
+
+	private static void writeUCSCEntityFile(List<UniProtReactomeEntry> uniProtReactomeEntries) throws IOException {
+		Path ucscEntityFilePath = Paths.get(outputDir, "ucsc_entity" + version);
+		Files.deleteIfExists(ucscEntityFilePath);
+		Files.createFile(ucscEntityFilePath);
+
+		String ucscEntityHeader =
+			"URL for entity_identifier: " + ReactomeConstants.UNIPROT_QUERY_URL +
+			System.lineSeparator() + System.lineSeparator() +
+			"Reactome Entity" +
+			System.lineSeparator() + System.lineSeparator();
+
+		Files.write(ucscEntityFilePath, ucscEntityHeader.getBytes(), StandardOpenOption.APPEND);
+		for (UniProtReactomeEntry uniProtReactomeEntry : uniProtReactomeEntries) {
+			Files.write(ucscEntityFilePath, uniProtReactomeEntry.getAccession().getBytes(), StandardOpenOption.APPEND);
+		}
+	}
+
+	private static void writeUCSCEventFile(Session graphDBSession, List<UniProtReactomeEntry> uniProtReactomeEntries)
+		throws IOException {
+		Path ucscEventFilePath = Paths.get(outputDir, "ucsc_events" + version);
+		Files.deleteIfExists(ucscEventFilePath);
+		Files.createFile(ucscEventFilePath);
+
+		Path ucscErrorFilePath = Paths.get(outputDir, "ucsc_" + version + ".err");
+		Files.deleteIfExists(ucscErrorFilePath);
+		Files.createFile(ucscErrorFilePath);
+
+		String ucscEventsHeader =
+			"URL for events: " + ReactomeConstants.PATHWAY_BROWSER_URL +
+			System.lineSeparator() + System.lineSeparator() +
+			String.join("\t", "Reactome Entity", "Event ST_ID", "Event_name") +
+			System.lineSeparator() + System.lineSeparator();
+
+		Files.write(ucscEventFilePath, ucscEventsHeader.getBytes(), StandardOpenOption.APPEND);
+		for (UniProtReactomeEntry uniProtReactomeEntry : uniProtReactomeEntries) {
+			Set<PathwayHierarchyUtilities.ReactomeEvent> reactomeEvents =
+				uniProtReactomeEntry.getEvents(graphDBSession);
+			if (reactomeEvents.isEmpty()) {
+				String errorMessage = uniProtReactomeEntry.getDisplayName() +
+									  " participates in Event(s) but no top Pathway can be found, " +
+									  " i.e. there seem to be a pathway" +
+									  " which contains or is an instance of itself.\n";
+
+				Files.write(ucscErrorFilePath, errorMessage.getBytes(), StandardOpenOption.APPEND);
+				continue;
+			}
+
+			for (PathwayHierarchyUtilities.ReactomeEvent reactomeEvent : reactomeEvents) {
+				String line = String.join("\t",
+					uniProtReactomeEntry.getAccession(),
+					reactomeEvent.getStableIdentifier(),
+					reactomeEvent.getName()
+				).concat(System.lineSeparator());
+
+				Files.write(ucscEventFilePath, line.getBytes(), StandardOpenOption.APPEND);
+			}
+		}
 	}
 
 	private static List<List<NCBIEntry>> splitList(List<NCBIEntry> list, int numOfSubLists) {
