4fc2d09c601905ab90738853ce3e467504e9543f
Joel Weiser <joel.weiser@oicr.on.ca>
2019-04-02 12:56:46 -0400

    adds test for sorting UniProt Reactome Entry objects

--- ncbi/src/test/java/org/reactome/release/UniProtReactomeEntryTest.java
+++ ncbi/src/test/java/org/reactome/release/UniProtReactomeEntryTest.java
@@ -5,8 +5,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.junit.Assert.*;
 
 public class UniProtReactomeEntryTest {
@@ -34,9 +40,21 @@ public class UniProtReactomeEntryTest {
 	}
 
 	@Test
+	public void sortsByUniProtAccessionAscendingly() {
+		List<UniProtReactomeEntry> uniProtReactomeEntries = new ArrayList<>();
+		uniProtReactomeEntries.add(uniProtReactomeEntry);
+		UniProtReactomeEntry uniProtReactomeEntry2 = UniProtReactomeEntry.get(5L, "A01234", "UniProt:A01234");
+		uniProtReactomeEntries.add(uniProtReactomeEntry2);
+
+		Collections.sort(uniProtReactomeEntries);
+
+		assertThat(uniProtReactomeEntries, contains(uniProtReactomeEntry2, uniProtReactomeEntry));
+	}
+
+	@Test
 	public void tenCharacterAccessionIsAccepted() {
 		final String ACCESSION = "P123456789";
-		UniProtReactomeEntry uniprot = UniProtReactomeEntry.get(5L, ACCESSION, "UniProt:test");
+		UniProtReactomeEntry uniprot = UniProtReactomeEntry.get(6L, ACCESSION, "UniProt:test");
 		assertThat(uniprot.getAccession() , equalTo(ACCESSION));
 	}
 
@@ -44,13 +62,13 @@ public class UniProtReactomeEntryTest {
 	public void incorrectAccessionThrowsIllegalArgumentException() {
 		expectedException.expect(IllegalArgumentException.class);
 		expectedException.expectMessage("not a proper UniProt Accession");
-		UniProtReactomeEntry.get(6L, "P123456", "UniProt:testing");
+		UniProtReactomeEntry.get(7L, "P123456", "UniProt:testing");
 	}
 
 	@Test
 	public void incorrectDisplayNameThrowsIllegalArgumentException() {
 		expectedException.expect(IllegalArgumentException.class);
 		expectedException.expectMessage("not a proper UniProt Display Name");
-		UniProtReactomeEntry.get(5L, "P12345", "testing");
+		UniProtReactomeEntry.get(8L, "P12345", "testing");
 	}
 }
