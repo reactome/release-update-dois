d0752f3d979b510fbca4d5e1c057f80a75215d9a
Joel Weiser <joel.weiser@oicr.on.ca>
2019-04-07 16:19:11 -0400

    initializes set as field and removes unneeded custom matcher

--- ncbi/src/test/java/org/reactome/release/UtilitiesTest.java
+++ ncbi/src/test/java/org/reactome/release/UtilitiesTest.java
@@ -1,8 +1,6 @@
 package org.reactome.release;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -12,44 +10,36 @@ import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.hasSize;
 
 public class UtilitiesTest {
+	private Set<String> stringSet;
+
+	@BeforeEach
+	public void initializeSet() {
+		this.stringSet = new HashSet<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
+	}
 
 	@Test
 	public void splitSetWithEvenSubSetSizes() {
-		Set<String> stringSet = getDummyStringSet();
+		final int NUM_OF_SUB_SETS = 4;
 
-		List<Set<String>> listOfStringSubSets = Utilities.splitSet(stringSet, 4);
+		List<Set<String>> listOfStringSubSets = Utilities.splitSet(stringSet, NUM_OF_SUB_SETS);
 
-		assertThat(listOfStringSubSets, hasSize(4));
-		assertThat(listOfStringSubSets, everyItem(hasSetSize(2)));
+		assertThat(listOfStringSubSets, hasSize(NUM_OF_SUB_SETS));
+
+		// Every set received even size of 2 elements
+		assertThat(listOfStringSubSets, everyItem(hasSize(2)));
 	}
 
 	@Test
 	public void splitSetWithUnEvenSubSetSizes() {
-		Set<String> stringSet = getDummyStringSet();
+		final int NUM_OF_SUB_SETS = 3;
 
-		List<Set<String>> listOfStringSubSets = Utilities.splitSet(stringSet, 3);
+		List<Set<String>> listOfStringSubSets = Utilities.splitSet(stringSet, NUM_OF_SUB_SETS);
 
-		assertThat(listOfStringSubSets, hasSize(3));
+		assertThat(listOfStringSubSets, hasSize(NUM_OF_SUB_SETS));
+
+		// Three sets of size 3, 3, and 2
 		assertThat(listOfStringSubSets.get(0), hasSize(3));
 		assertThat(listOfStringSubSets.get(1), hasSize(3));
 		assertThat(listOfStringSubSets.get(2), hasSize(2));
-	}
-
-	private Set<String> getDummyStringSet() {
-		return new HashSet<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
-	}
-
-	private Matcher<Set<String>> hasSetSize(int size) {
-		return new TypeSafeMatcher<Set<String>>() {
-			@Override
-			protected boolean matchesSafely(Set<String> item) {
-				return item.size() == size;
-			}
-
-			@Override
-			public void describeTo(Description description) {
-				description.appendText("size of string set is " + size);
-			}
-		};
 	}
 }
