d8715c24407577676cb514784cf9ffa8afa421c4
Joel Weiser <joel.weiser@oicr.on.ca>
2019-04-08 10:51:34 -0400

    adds documentation for NCBIProtein class

--- ncbi/src/main/java/org/reactome/release/NCBIProtein.java
+++ ncbi/src/main/java/org/reactome/release/NCBIProtein.java
@@ -15,6 +15,12 @@ import java.util.stream.Collectors;
 
 import static org.reactome.release.Utilities.appendWithNewLine;
 
+/**
+ * File generator for NCBI Protein.  This class has logic for producing a file for
+ * NCBI Protein, listing the UniProt entries in Reactome which are associated with an
+ * NCBI Gene identifier (these are not listed in the file).
+ * @author jweiser
+ */
 public class NCBIProtein {
 	private static final Logger logger = LogManager.getLogger();
 
@@ -32,8 +38,12 @@ public class NCBIProtein {
 		this.reactomeVersion = reactomeVersion;
 	}
 
-	public void writeNCBIProteinFile()
-		throws IOException {
+	/**
+	 * Writes an NCBI Protein file describing the UniProt entries in Reactome with an NCBI Gene identifier (not listed)
+	 * to a pre-set output directory
+	 * @throws IOException Thrown if creating or appending for file fails
+	 */
+	public void writeNCBIProteinFile() throws IOException {
 		Path ncbiProteinFilePath = getNCBIProteinFilePath();
 		Files.deleteIfExists(ncbiProteinFilePath);
 		Files.createFile(ncbiProteinFilePath);
