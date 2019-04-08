f04d902062be5ea53600fefcddd57caf7c07b721
Joel Weiser <joel.weiser@oicr.on.ca>
2019-04-08 12:01:25 -0400

    adds documentation to Utilities class

--- ncbi/src/main/java/org/reactome/release/Utilities.java
+++ ncbi/src/main/java/org/reactome/release/Utilities.java
@@ -6,8 +6,20 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
 
+/**
+ * General Utilities class.  This class contains methods needed for general manipulation of data or common operations
+ * used by the data export process.
+ * @author jweiser
+ */
 public class Utilities {
-
+	/**
+	 * Takes a set of Strings and splits it into a list of the requested number of sub-sets.  If an ordered set, order
+	 * is preserved.  If the set does not split evenly into the number of set-sets requested, the remainder elements
+	 * will be added one element each to the beginning sets of the returned list.
+	 * @param set Set of Strings to split
+	 * @param numOfSubSets Number of sub-sets to return
+	 * @return List of String sub-sets
+	 */
 	public static List<Set<String>> splitSet(Set<String> set, int numOfSubSets) {
 		int subSetSize = set.size() / numOfSubSets ;
 		int numberOfExtraKeys = set.size() % numOfSubSets;
@@ -42,6 +54,12 @@ public class Utilities {
 		return splitSets;
 	}
 
+	/**
+	 * Appends a String value to a file, specified by path, with a new line character (determined by OS)
+	 * @param lineToAppend Line to append to file
+	 * @param filePath Path of file to which to append line
+	 * @throws IOException Thrown if unable to append to file at path provided
+	 */
 	public static void appendWithNewLine(String lineToAppend, Path filePath) throws IOException {
 		Files.write(
 			filePath,
@@ -50,6 +68,14 @@ public class Utilities {
 		);
 	}
 
+
+	/**
+	 * Appends a List of String values to a file, specified by path, with a new line character (determined by OS)
+	 * added for each String value in the list
+	 * @param linesToAppend Lines to append to file
+	 * @param filePath Path of file to which to append line
+	 * @throws IOException Thrown if unable to append to file at path provided
+	 */
 	public static void appendWithNewLine(Iterable<String> linesToAppend, Path filePath) throws IOException {
 		for (String lineToAppend : linesToAppend) {
 			appendWithNewLine(lineToAppend, filePath);
