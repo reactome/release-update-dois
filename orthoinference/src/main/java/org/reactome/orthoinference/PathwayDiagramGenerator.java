cc75ef854689d966373991e2989f397f82e6ff1e
Justin Cook <cookersjs@gmail.com>
2019-04-24 16:10:30 -0700

    Added comments

--- orthoinference/src/main/java/org/reactome/orthoinference/PathwayDiagramGenerator.java
+++ orthoinference/src/main/java/org/reactome/orthoinference/PathwayDiagramGenerator.java
@@ -17,22 +17,33 @@ public class PathwayDiagramGenerator {
     private static GKInstance speciesInst;
     private static long personId;
     private static long referenceSpeciesId;
+
+    /**
+     * This method will go through all reference species PathwayDiagrams and finds the orthologous Pathway instances that are for the current target species and
+     * generates the orthologous PathwayDiagram via the PredictedPathwayDiagramGeneratorFromDB method in CuratorTool.
+     * @throws Exception
+     */
     public static void generateOrthologousPathwayDiagrams() throws Exception {
 
         logger.info("Generating diagrams for inferred " + speciesInst.getDisplayName() + "pathways");
+        // Create PredictedPathwayDiagramGeneratorFromDB object and set db adaptor and author ID.
         PredictedPathwayDiagramGeneratorFromDB diagramGenerator = new PredictedPathwayDiagramGeneratorFromDB();
         diagramGenerator.setMySQLAdaptor(dba);
         diagramGenerator.setDefaultPersonId(personId);
 
         GKInstance referenceSpeciesInst = dba.fetchInstance(referenceSpeciesId);
 
+        // Iterate through each PathwayDiagram instance looking for those associated with the reference species.
         for (GKInstance diagramInst: (Collection<GKInstance>) dba.fetchInstancesByClass(ReactomeJavaConstants.PathwayDiagram)) {
             GKInstance pathwayInst = (GKInstance) diagramInst.getAttributeValue(ReactomeJavaConstants.representedPathway);
             GKInstance sourceSpeciesInst = (GKInstance) pathwayInst.getAttributeValue(ReactomeJavaConstants.species);
             if (sourceSpeciesInst == referenceSpeciesInst) {
+                // When a PathwayDiagram instance associated with the reference species is found, iterate through all of it's OrthologousEvent instances.
                 for (GKInstance orthoPathwayInst : (Collection<GKInstance>) pathwayInst.getAttributeValuesList(ReactomeJavaConstants.orthologousEvent)) {
                     GKInstance targetSpeciesInst = (GKInstance) orthoPathwayInst.getAttributeValue(ReactomeJavaConstants.species);
+                    // Look for OrthologousEvent instances that match the current target species and that are electronically inferred.
                     if (targetSpeciesInst == speciesInst && orthoPathwayInst.getAttributeValue(ReactomeJavaConstants.evidenceType) != null) {
+                        // Generate Orthologous PathwayDiagram instance using generatePredictedDiagram method from PredictedPathwayDiagramGeneratorFromDB.
                         logger.info("Building Pathway diagram for " + orthoPathwayInst);
                         diagramGenerator.generatePredictedDiagram(orthoPathwayInst, pathwayInst, diagramInst);
                     }
