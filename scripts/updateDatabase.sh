342b28160a1c370022cf20a4d20cf7f3062de274
cookersjs <cookersjs@gmail.com>
2019-04-29 11:24:07 -0400

    Arguments and cleanup for bash script

--- scripts/updateDatabase.sh
+++ scripts/updateDatabase.sh
@@ -1,54 +1,80 @@
 #!/bin/bash
 
+## This script will create or replace a database using a mysql dump file. It takes the DB name and dump filepath as arguements.
+## A config file populated with mySQL username and password is also required.
+## author: jcook
 
-if [ "$#" -gt 1 ]
+dbFilepath=
+dbName=
+configFilepath=config.properties
+## Parse execution arguments
+while (( "$#" )); do
+	case "$1" in
+		-f|--file)
+			dbFilepath=$2;
+			shift 2;
+			;;
+		-d|--database)
+			dbName=$2;
+			shift 2;
+			;;
+		-c|--config)
+			configFilepath=$2;
+			shift 2;
+			;;
+		-*|--*=)
+			echo "Error: Unsupported flag $1"
+			exit 1
+	esac
+done
+
+## If missing arguments, explain usage
+if [ -z "$dbFilepath" ] || [ -z "$dbName" ]
 then
-	dbName=$1
-	dbFilepath=$2
-	configFilepath=config.properties
-	if [ ! -z $3 ] 
-	then
-		configFilepath=$3
-	fi
-
-	echo "Reading $configFilepath"
-	
-	username=
-	password=
-	## Parse config file for username and password properties
-	while read line
-	do
-		if [[ $line == username* ]]
-		then
-			username=${line#*=}
-		elif [[ $line == password* ]]
-		then
-			password=${line#*=}
-		fi
-	done < $configFilepath
-
-	echo "Updating $1 with $2"
-	echo "Backing up $1"
-	## Take archive of DB, drop it and create a new, empty one
-	mysqldump -u$username -p$password $1 > $1.dump
-	echo "Finished backing up $1"
-	echo "Updating $1 with $2"
-	mysql -u$username -p$password -e 'drop database if exists $1; create database $1'
-
-	## If dump is gzipped, must use 'zcat'
-	catCommand=cat
-	if [ $2 == *.gz ]
-	then 
-		catCommand=zcat
-	fi
-	## Restore database using 'cat' piped to mysql
-	cmd="$catCommand $2 | mysql -u$username -p$password $1"
-	echo "Restoring updated $1"
-	eval $cmd
-	echo "Finished database update"
-else
-	echo "Please provide database name and path to the dump file"
+	echo "Create or replace mySQL databases";
+	echo "Usage: bash updateDatabase.sh -f databaseFile -d databaseName [-c configFile] ";
+	exit 1
 fi
 
+## Parse config file for username and password properties
+echo "Reading $configFilepath";
+username=
+password=
+while read line; do
+	if [[ $line == username* ]]
+ 	then
+ 		username=${line#*=}
+ 	elif [[ $line == password* ]]
+ 	then
+ 		password=${line#*=}
+ 	fi
+done < $configFilepath
 
+## Improperly formatted or missing config file
+if [ -z "$username" ] || [ -z "$password" ]
+then
+	echo "Could not find username and/or password in $configFilepath";
+	exit 1
+fi
 
+echo "Updating $dbName with $dbFilepath";
+echo "Backing up $dbName";
+
+## Take archive of DB, drop it and create a new, empty one
+mysqldump -u$username -p$password $dbName > $dbName.backup.dump
+echo "Finished backing up $dbName";
+echo "Updating $dbName with $dbFilepath";
+mysql -u$username -p$password -e 'drop database if exists $dbName; create database $dbName'
+
+## If dump is gzipped, must use 'zcat'
+catCommand=cat
+if [ $dbFilepath == *.gz ]
+then
+	catCommand=zcat
+fi
+
+## Restore database using 'cat' piped to mysql
+cmd="$catCommand $dbFilepath | mysql -u$username -p$password $dbName"
+echo "Restoring updated $dbName";
+eval $cmd;
+echo "Finished database update";
