7f3f9a511dd3cff2695a8c494c03fd0db66c39d1
cookersjs <cookersjs@gmail.com>
2019-05-28 12:27:55 -0400

    Unit tests for OrthoDiagrams

--- orthoinference/src/test/java/org/reactome/orthoinference/OrthologousPathwayDiagramGeneratorTest.java
+++ orthoinference/src/test/java/org/reactome/orthoinference/OrthologousPathwayDiagramGeneratorTest.java
@@ -21,6 +21,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({OrthologousPathwayDiagramGenerator.class, PredictedPathwayDiagramGeneratorFromDB.class})
 @PowerMockIgnore({"org.apache.logging.log4j.*", "javax.management.*", "javax.script.*",
@@ -33,6 +35,8 @@ public class OrthologousPathwayDiagramGeneratorTest {
     MySQLAdaptor mockPrevAdaptor;
     @Mock
     GKInstance mockSpeciesInst;
+    @Mock
+    GKInstance mockSpeciesInst2;
     @Mock
     GKInstance mockAlternateSpeciesInst;
     @Mock
@@ -92,6 +96,69 @@ public class OrthologousPathwayDiagramGeneratorTest {
         Mockito.when(mockDiagramInst.getAttributeValue(ReactomeJavaConstants.representedPathway)).thenReturn(mockPathwayInst);
         Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockAlternateSpeciesInst);
         testDiagramGenerator.generateOrthologousPathwayDiagrams();
+    }
 
+    @Test
+    public void sameSpeciesReturnsTrue() throws Exception {
+        Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockSpeciesInst);
+        assertTrue(testDiagramGenerator.isSameSpecies(mockPathwayInst, mockSpeciesInst));
+    }
+
+    @Test
+    public void differentSpeciesReturnsFalse() throws Exception {
+        Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockSpeciesInst2);
+        assertFalse(testDiagramGenerator.isSameSpecies(mockPathwayInst, mockSpeciesInst));
+    }
+
+    @Test
+    public void electroniciallyInferredReturnsTrue() throws Exception {
+        Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.evidenceType)).thenReturn(mockEvidenceTypeInst);
+        assertTrue(testDiagramGenerator.isElectronicallyInferred(mockPathwayInst));
+    }
+
+    @Test
+    public void manuallyInferredReturnsFalse() throws Exception {
+        Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.evidenceType)).thenReturn(null);
+        assertFalse(testDiagramGenerator.isElectronicallyInferred(mockPathwayInst));
+    }
+
+    @Test
+    public void fewerDiagramCountsReturnsTrue() {
+        assertTrue(testDiagramGenerator.hasFewerSpeciesDiagramCountsBetweenReleases(4, 6));
+    }
+
+    @Test
+    public void equalDiagramCountsReturnsFalse() {
+        assertFalse(testDiagramGenerator.hasFewerSpeciesDiagramCountsBetweenReleases(4, 4));
+    }
+
+    @Test
+    public void moreDiagramCountsReturnsFalse() {
+        assertFalse(testDiagramGenerator.hasFewerSpeciesDiagramCountsBetweenReleases(6, 4));
+    }
+
+    @Test
+    public void differentSpeciesDoesNotProducePathwayDiagram() throws Exception {
+        Mockito.when(mockOrthoEventInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockSpeciesInst2);
+        Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockSpeciesInst);
+        Mockito.when(mockOrthoEventInst.getAttributeValue(ReactomeJavaConstants.evidenceType)).thenReturn(mockEvidenceTypeInst);
+        Mockito.when(mockDiagramGenerator.generatePredictedDiagram(mockOrthoEventInst, mockPathwayInst, mockDiagramInst)).thenReturn(mockOrthoDiagramInst);
+        assertNull(testDiagramGenerator.generateOrthologousPathwayDiagram(mockOrthoEventInst, mockPathwayInst, mockDiagramInst, mockDiagramGenerator));
+    }
+
+    @Test
+    public void manuallyInferredPathwayDoesNotProducePathwayDiagram() throws Exception {
+        Mockito.when(mockOrthoEventInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockSpeciesInst);
+        Mockito.when(mockPathwayInst.getAttributeValue(ReactomeJavaConstants.evidenceType)).thenReturn(null);
+        Mockito.when(mockDiagramGenerator.generatePredictedDiagram(mockOrthoEventInst, mockPathwayInst, mockDiagramInst)).thenReturn(mockOrthoDiagramInst);
+        assertNull(testDiagramGenerator.generateOrthologousPathwayDiagram(mockOrthoEventInst, mockPathwayInst, mockDiagramInst, mockDiagramGenerator));
+    }
+
+    @Test
+    public void sameSpeciesAndElectronicallyInferredProducesPathwayDiagram() throws Exception {
+        Mockito.when(mockOrthoEventInst.getAttributeValue(ReactomeJavaConstants.species)).thenReturn(mockSpeciesInst);
+        Mockito.when(mockOrthoEventInst.getAttributeValue(ReactomeJavaConstants.evidenceType)).thenReturn(mockEvidenceTypeInst);
+        Mockito.when(mockDiagramGenerator.generatePredictedDiagram(mockOrthoEventInst, mockPathwayInst, mockDiagramInst)).thenReturn(mockOrthoDiagramInst);
+        assertEquals(mockOrthoDiagramInst, testDiagramGenerator.generateOrthologousPathwayDiagram(mockOrthoEventInst, mockPathwayInst, mockDiagramInst, mockDiagramGenerator));
     }
 }
