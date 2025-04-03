97df91737ce6e278b74d855f7aa9d1fb31fdfc7e
jweiser <joel.weiser@oicr.on.ca>
2025-04-02 23:38:32 -0400

    updates tests

--- src/test/java/org/reactome/release/updateDOIs/TestUpdateDOIs.java
+++ src/test/java/org/reactome/release/updateDOIs/TestUpdateDOIs.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.gk.model.GKInstance;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.reactome.server.service.model.GKInstance;
+import org.reactome.server.service.model.ReactomeJavaConstants;
 import org.reactome.server.service.persistence.Neo4JAdaptor;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -19,6 +20,9 @@ public class TestUpdateDOIs {
 
 	@Mock
 	private Neo4JAdaptor mockAdaptor;
+
+	@Mock
+	private PropertyManager mockPropertyManager;
 
 	@Mock
 	private GKInstance instance;
@@ -29,8 +33,8 @@ public class TestUpdateDOIs {
 
 	@Test
 	public void testfindNewDOIsAndUpdate() throws Exception {
-		UpdateDOIs check = new UpdateDOIs();
-		check.setAdaptors(mockAdaptor, mockAdaptor);
+		setUpMockPropertyManager();
+		UpdateDOIs check = new UpdateDOIs(mockPropertyManager);
 
 		Collection<GKInstance> testResults = Arrays.asList(instance);
 		Mockito.when((GKInstance) instance.getAttributeValue("stableIdentifier"))
@@ -38,51 +42,56 @@ public class TestUpdateDOIs {
 		Mockito.when(instance.getDisplayName())
 			.thenReturn("HSA-12345");
 
-		Mockito.when(instance.getAttributeValue("name"))
-			.thenReturn("Name Value");
-		Mockito.when(instance.getAttributeValue("DB_ID"))
-			.thenReturn("67890");
+		Mockito.when(instance.getDBID())
+			.thenReturn(67890L);
 
-		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "doi", "NOT REGEXP", "^10.3180"))
-			.thenReturn(testResults);
-		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "DB_ID", "=", "67890"))
+		Mockito.when(mockAdaptor.fetchInstancesByClass(ReactomeJavaConstants.Pathway))
 			.thenReturn(testResults);
 
-		check.findAndUpdateDOIs(MOCK_PERSON_DBID, MOCK_REPORT_PATH, MOCK_RELEASE_NUMBER, true);
+		check.findAndUpdateDOIs(MOCK_REPORT_PATH.toString());
 	}
 
 	@Test
 	public void testfindNewDOIsAndUpdateEmptyList() throws Exception {
-		UpdateDOIs check = new UpdateDOIs();
-		check.setAdaptors(mockAdaptor, mockAdaptor);
+		setUpMockPropertyManager();
+		UpdateDOIs check = new UpdateDOIs(mockPropertyManager);
 
-		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "doi", "NOT REGEXP", "^10.3180"))
-			.thenReturn(new ArrayList<GKInstance>());
-		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "DB_ID", "=", "67890"))
+		Mockito.when(mockAdaptor.fetchInstancesByClass(ReactomeJavaConstants.Pathway))
 			.thenReturn(new ArrayList<GKInstance>());
 
-		check.findAndUpdateDOIs(MOCK_PERSON_DBID, MOCK_REPORT_PATH, MOCK_RELEASE_NUMBER, true);
+		check.findAndUpdateDOIs(MOCK_REPORT_PATH.toString());
 	}
 
 	@Test
 	public void testfindNewDOIsAndUpdateDeepEmptyList() throws Exception {
-		UpdateDOIs check = new UpdateDOIs();
-		check.setAdaptors(mockAdaptor, mockAdaptor);
+		setUpMockPropertyManager();
+		UpdateDOIs check = new UpdateDOIs(mockPropertyManager);
 
 		Collection<GKInstance> testResults = Arrays.asList(instance);
 
 		Mockito.when((GKInstance) instance.getAttributeValue("stableIdentifier"))
 			.thenReturn(instance);
-		Mockito.when(instance.getAttributeValue("name"))
+		Mockito.when(instance.getDisplayName())
 			.thenReturn("Name Value");
-		Mockito.when(instance.getAttributeValue("DB_ID"))
-			.thenReturn("67890");
+		Mockito.when(instance.getDBID())
+			.thenReturn(67890L);
 
-		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "doi", "NOT REGEXP", "^10.3180"))
+		Mockito.when(mockAdaptor.fetchInstancesByClass(ReactomeJavaConstants.Pathway))
 			.thenReturn(testResults);
-		Mockito.when(mockAdaptor.fetchInstanceByAttribute("Pathway", "DB_ID", "=", "67890"))
-			.thenReturn(new ArrayList<GKInstance>());
 
-		check.findAndUpdateDOIs(MOCK_PERSON_DBID, MOCK_REPORT_PATH, MOCK_RELEASE_NUMBER,true);
+		check.findAndUpdateDOIs(MOCK_REPORT_PATH.toString());
+	}
+
+	private void setUpMockPropertyManager() {
+		Mockito.when(mockPropertyManager.getGKCentralDbAdaptor())
+			.thenReturn(mockAdaptor);
+		Mockito.when(mockPropertyManager.getReleaseDbAdaptor())
+			.thenReturn(mockAdaptor);
+		Mockito.when(mockPropertyManager.getPersonId())
+			.thenReturn(MOCK_PERSON_DBID);
+		Mockito.when(mockPropertyManager.getReleaseNumber())
+			.thenReturn(MOCK_RELEASE_NUMBER);
+		Mockito.when(mockPropertyManager.getTestMode())
+			.thenReturn(true);
 	}
 }
